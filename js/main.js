394a242d68af0d9a3f06e653382636a87b572d6b
akbarinfo <girtbezbet@gmail.com>
2022-01-17 00:48:20 +0500

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
-document.getElementById('color').addEventListener('click', function()
+// document.getElementById('color').addEventListener('click', function()
+// {
+//   document.body.classList.toggle('dark')
+// } )
+
+let rightbtn = document.getElementById('right-click');
+
+let leftbtn = document.getElementById('left-click');
+
+let color = document.getElementById('fx-color');
+
+leftbtn.addEventListener(
+  'click', function(){
+    color.classList.add('show');
+  }
+);
+
+rightbtn.addEventListener('click', function() {
+  color.classList.remove('show');
+});
+
+document.getElementById('id-dark').addEventListener('click', function()
 {
   document.body.classList.toggle('dark')
+} )
+
+document.getElementById('id-light').addEventListener('click', function()
+{
+  document.body.classList.toggle('root')
 } )
